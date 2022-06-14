741ac4d0969ec68a0f60d70584713cf21b50fe15
soopiri <soopiri.jay@gmail.com>
2022-06-15 01:03:52 +0900

    ✨ Climb Sheep

---
+++ climb_sheep/app.js
@@ -0,0 +1,59 @@
+import { Hill } from "./hill.js";
+import { SheepController } from "./sheep-control.js";
+
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
+
+    this.hills = [
+      new Hill("#fd6bea", 0.2, 12),
+      new Hill("#ff59c2", 0.5, 8),
+      new Hill("#ff4674", 1.4, 6),
+    ];
+
+    this.sheepController = new SheepController();
+
+    // to get screen size
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    // for retina display
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
+
+    for (let i = 0; i < this.hills.length; i++) {
+      this.hills[i].resize(this.stageWidth, this.stageHeight);
+    }
+
+    this.sheepController.resize(this.stageWidth, this.stageHeight);
+  }
+
+  animate(t) {
+    requestAnimationFrame(this.animate.bind(this));
+
+    // clear canvas
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    let dots;
+    for (let i = 0; i < this.hills.length; i++) {
+      dots = this.hills[i].draw(this.ctx);
+    }
+
+    // t: fps를 위한 timestamp
+    this.sheepController.draw(this.ctx, t, dots);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
